48468b8e8706887633570307e953587d26521c8a
Bikash Vishwakarma <srvsingh003@gmail.com>
2024-01-09 08:54:00 +0530

    added the personal detail in the accounts section

--- src/Components/uiGroup/Accountsettings/personalDetails.js
+++ src/Components/uiGroup/Accountsettings/personalDetails.js
@@ -36,13 +36,47 @@ function PersonalDetails() {
           />
         </div>
       </div>
-      {/* <input type="text" className=" border" />
-      <input type="text" className=" border" />
-      <input type="text" className=" border" />
-      <div>
-        <button>sdfsdfn</button>
-        <button>sdgfuysdkt</button>
-      </div> */}
+      <div className=" flex flex-col mt-6">
+        <h3 className=" text-[14px] font-medium  font-Poppins text-[#344054]">
+          Email
+        </h3>
+        <input
+          type="text"
+          className=" border  w-auto"
+          style={{ padding: "10px 14px 10px 14px" }}
+        />
+      </div>
+      <div className=" flex flex-col mt-6">
+        <h3 className=" text-[14px] font-medium  font-Poppins text-[#344054]">
+          Phone
+        </h3>
+        <input
+          type="text"
+          className=" border  w-auto"
+          style={{ padding: "10px 14px 10px 14px" }}
+        />
+      </div>
+      <div className=" flex flex-col mt-6">
+        <h3 className=" text-[14px] font-medium  font-Poppins text-[#344054]">
+          Description
+        </h3>
+        <input
+          type="text"
+          className=" border  w-auto"
+          style={{ padding: "10px 14px 10px 14px" }}
+        />
+      </div>
+
+      <div className=" flex justify-end mt-6 items-center">
+        <div className=" p-1  bg-slate-200 mr-4 rounded-lg">
+          <button className=" border h-[36px] w-[117px] bg-[#3674CB] text-white text-[12px] font-semibold leading-5 rounded-lg ">
+            Save Changes
+          </button>
+        </div>
+        <button className=" border h-[36px] w-[117px]   text-gray-400 text-[12px] font-semibold leading-5 rounded-lg">
+          Cancel
+        </button>
+      </div>
     </div>
   );
 }
