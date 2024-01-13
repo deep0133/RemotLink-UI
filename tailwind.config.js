0824f7f1c66763f4815f02043a618c651e145b10
saurabh singh <srvsingh003@gmail.com>
2024-01-13 10:02:24 +0530

    added daisy ui nd chnaged tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,43 +1,38 @@
 /** @type {import('tailwindcss').Config} */
-const { createThemes } = require('tw-colors');
-
+const { createThemes } = require("tw-colors");
+const daisyui = require("daisyui");
 
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-    "./node_modules/flowbite/**/*.js",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}", "./node_modules/flowbite/**/*.js"],
   theme: {
     extend: {
       fontFamily: {
         AnekLatin: ["Anek Latin", "sans-serif"],
         Lusitana: ["Lusitana", "serif"],
         Poppins: ["Poppins", "serif"],
-        Sora: ['Sora', "sans-serif"],
-        Outfit: ['Outfit', "sans-serif"],
-        SourceSans: ['Source Sans Pro', "sans-serif"],
-        FiraSans: ['Fira Sans', "sans-serif"],
+        Sora: ["Sora", "sans-serif"],
+        Outfit: ["Outfit", "sans-serif"],
+        SourceSans: ["Source Sans Pro", "sans-serif"],
+        FiraSans: ["Fira Sans", "sans-serif"],
+      },
+      borderColor: ["hover"],
+      scale: ["hover"],
     },
-    borderColor: ['hover'],
-      scale: ['hover'],
-   
-    
-  }
   },
   plugins: [
+    daisyui,
     createThemes({
-                 light: { 
-                   'primary': '#FFFFFF',
-                    'secondary': '#323232',
-                    'brand': '#FFFFFFAD',
-                    'btn': '#222222' ,
-                 },
-                 purple: { 
-                  'primary': '#FFFFFF',
-                   'secondary': '#3D1766',
-                   'brand': '#25014C',
-                   
-                }
-              })
+      light: {
+        primary: "#FFFFFF",
+        secondary: "#323232",
+        brand: "#FFFFFFAD",
+        btn: "#222222",
+      },
+      purple: {
+        primary: "#FFFFFF",
+        secondary: "#3D1766",
+        brand: "#25014C",
+      },
+    }),
   ],
-}
+};
